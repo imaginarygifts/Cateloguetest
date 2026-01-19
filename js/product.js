770a241b1dd49bcd04b56488241a2311f4b51ef2
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-01-19 12:54:33 +0530

    Update product.js

--- js/product.js
+++ js/product.js
@@ -297,8 +297,13 @@ window.buyNow = function () {
     name: "Imaginary Gifts",
     description: product.name,
     image: product.images?.[0] || "",
-    handler: function (response) {
-      alert("Payment Successful!");
+   handler: function (response) {
+  const msg = `Payment Successful!\n\nProduct: ${product.name}\nAmount: ₹${finalPrice}\nPayment ID: ${response.razorpay_payment_id}`;
+
+  const waUrl = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(msg)}`;
+
+  window.location.href = waUrl;
+}
 
       let msg = `✅ Payment Received\n\n`;
       msg += `Product: ${product.name}\n`;
